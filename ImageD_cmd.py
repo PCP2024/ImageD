f33bdb047b463dfa26edd5371dabd601a8081918
aitanavs <aitanaverdu@gmail.com>
2024-05-26 16:52:42 +0200

    changes to cmd

--- ImageD_cmd.py
+++ ImageD_cmd.py
@@ -14,7 +14,8 @@ def main():
         program_version = f.readline()
     
     parser.add_argument("--version", action = "version", version = program_version)
-    parser.add_argument("input_image", help="file path")
+    parser.add_argument("input_image", help="file path for input image 1")
+    parser.add_argument("input_image2", help="file path for input image 2")
     parser.add_argument("-o","--output_image", help="file path for output image")
 
     parser.add_argument("--brightness", type=float, help="""
@@ -24,17 +25,56 @@ def main():
         Returns:
             brightness adjusted image.
         """)
+    parser.add_argument("--contrast", type=float, help="""
+        Adjusts image contrast.
+        Args:
+            param: contrast parameter. 0: full grey, 1: same, >1: more contrast.
+        Returns:
+            contrast adjusted image.
+        """)
+    parser.add_argument("--color", type = float, help = """
+        Adjusts image colors.
+        Args:
+            param: color parameter. 0: black and white, 1: same, >1: enhanced colors.
+        Returns:
+            color adjusted image.
+        """)
+
+    parser.add_argument("--sharpness", type = float, help = """
+        Adjusts image sharpness.
+        Args:
+            param: sharpness parameter. 0: blur, 1: same, >1: sharpened image.
+        Returns:
+            sharpness adjusted image.
+        """)
+
     
     args = parser.parse_args()
 
-    # Load image
+    # Load input images 1 and 2
     imload_instance = imaged.dataio.ImageLoader(args.input_image)
     image = imload_instance.load_image()
+
+    if args.input_image2 is not None:
+        imload_instance2 = imaged.dataio.ImageLoader(args.input_image2)
+        image2 = imload_instance.load_image()
 
     # Process image
     if args.brightness is not None:
         process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
         image = process_instance.brightness(args.brightness)
+
+    if args.contrast is not None:
+        process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
+        image = process_instance.contrast(args.contrast)
+
+    if args.color is not None:
+        process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
+        image = process_instance.contrast(args.color)
+
+    if args.sharpness is not None:
+        process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
+        image = process_instance.contrast(args.sharpness)
 
     # output
     if args.output_image is not None:
