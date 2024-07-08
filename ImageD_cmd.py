842fb33f73766d89ecd195dce245d712718d7ac3
ananaphasia <asimonoff1101@gmail.com>
2024-07-08 13:04:20 +0200

    fix typo in CLI

--- ImageD_cmd.py
+++ ImageD_cmd.py
@@ -108,26 +108,31 @@ def main():
 
     # Process image
     if args.remove_background:
-        image = imaged.processing.ImageProcessing.remove_background(image_
+        image = imaged.processing.ImageProcessing.remove_background(image)
 
     if args.brightness is not None:
-        process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
+        process_instance = imaged.processing.ImageProcessing.ImageProcessor(
+            image)
         image = process_instance.brightness(args.brightness)
 
     if args.contrast is not None:
-        process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
+        process_instance = imaged.processing.ImageProcessing.ImageProcessor(
+            image)
         image = process_instance.contrast(args.contrast)
 
     if args.color is not None:
-        process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
+        process_instance = imaged.processing.ImageProcessing.ImageProcessor(
+            image)
         image = process_instance.contrast(args.color)
 
     if args.sharpness is not None:
-        process_instance = imaged.processing.ImageProcessing.ImageProcessor(image)
+        process_instance = imaged.processing.ImageProcessing.ImageProcessor(
+            image)
         image = process_instance.contrast(args.sharpness)
 
     if args.resize is not None:
-        image = imaged.processing.ImageProcessing.resize(image, tuple(args.resize))
+        image = imaged.processing.ImageProcessing.resize(
+            image, tuple(args.resize))
         image.show()
 
     if args.rotate is not None:
