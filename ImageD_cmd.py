538ecac9ba9be53d4ed39e331cafd75fea2e33f8
ananaphasia <asimonoff1101@gmail.com>
2024-07-08 23:19:07 +0200

    add config to cmd

--- ImageD_cmd.py
+++ ImageD_cmd.py
@@ -4,9 +4,13 @@ import PIL
 import imaged.dataio
 import imaged.processing
 import imaged.processing.ImageProcessing
+import json
 
 
 def main():
+    config = json.load(open("config.json"))
+    config = config.get("CMD", None)[0]
+
     parser = argparse.ArgumentParser(
         description="Hello! Thank you for using ImageD. We hope you have a pleasant experience. Enjoy!",
         epilog="Goodbye, thank you for using ImageD. You will be missed. xoxo",
@@ -107,42 +111,56 @@ def main():
     imload_instance.show_image(image)
 
     # Process image
-    if args.remove_background:
+    if args.remove_background or config.get("remove_background", False):
         image = imaged.processing.ImageProcessing.remove_background(image)
 
-    if args.brightness is not None:
+    if args.brightness is not None or config.get("brightness", None) is not None:
+        brightness = args.brightness if args.brightness is not None else config.get(
+            "brightness", None)
         process_instance = imaged.processing.ImageProcessing.ImageProcessor(
             image)
-        image = process_instance.brightness(args.brightness)
+        image = process_instance.brightness(brightness)
 
-    if args.contrast is not None:
+    if args.contrast is not None or config.get("contrast", None) is not None:
+        contrast = args.contrast if args.contrast is not None else config.get(
+            "contrast", None)
         process_instance = imaged.processing.ImageProcessing.ImageProcessor(
             image)
-        image = process_instance.contrast(args.contrast)
+        image = process_instance.contrast(contrast)
 
-    if args.color is not None:
+    if args.color is not None or config.get("color", None) is not None:
+        color = args.color if args.color is not None else config.get(
+            "color", None)
         process_instance = imaged.processing.ImageProcessing.ImageProcessor(
             image)
-        image = process_instance.contrast(args.color)
+        image = process_instance.contrast(color)
 
-    if args.sharpness is not None:
+    if args.sharpness is not None or config.get("sharpness", None) is not None:
+        sharpness = args.sharpness if args.sharpness is not None else config.get(
+            "sharpness", None)
         process_instance = imaged.processing.ImageProcessing.ImageProcessor(
             image)
-        image = process_instance.contrast(args.sharpness)
+        image = process_instance.contrast(sharpness)
 
-    if args.resize is not None:
+    if args.resize is not None or config.get("resize", None) is not None:
+        resize = args.resize if args.resize is not None else config.get(
+            "resize", None)
         image = imaged.processing.ImageProcessing.resize(
-            image, tuple(args.resize))
+            image, tuple(resize))
         image.show()
 
-    if args.rotate is not None:
-        image = imaged.processing.ImageProcessing.rotate(image, args.rotate)
+    if args.rotate is not None or config.get("rotate", None) is not None:
+        rotate = args.rotate if args.rotate is not None else config.get(
+            "rotate", None)
+        image = imaged.processing.ImageProcessing.rotate(image, rotate)
         image.show()
 
     # output ()
-    if args.output_image is not None:
-        output_instance = imaged.dataio.ImageLoader(args.output_image)
-        output_instance.save_image(image, save_path=args.output_image)
+    if args.output_image is not None or config.get("output_image", None) is not None:
+        output_image = args.output_image if args.output_image is not None else config.get(
+            "output_image", None)
+        output_instance = imaged.dataio.ImageLoader(output_image)
+        output_instance.save_image(image, save_path=output_image)
 
     # image = PIL.Image.open(args.input_image)
     # parser.add_argument("input_image", type=argparse.FileType('r'))
